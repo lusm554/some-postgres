00380a4ba27ebdba9c1363ffcc48fdd468bb883f
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-22 00:15:40 +0300

    create metadata table and fill it

---
+++ utils/fill_metadata.sql
@@ -0,0 +1,24 @@
+/*create table metadata (
+  id serial primary key, 
+  name text,
+  age integer,
+  address text,
+  phone text
+);
+
+insert into metadata (name, age, address, phone)
+values ('Nikita', 17, '1332 Denver Avenue', '951-436-1962');
+
+insert into metadata (name, age, address, phone)
+values ('Liam', 22, '2563 Farm Meadow Drive', '931-221-4429');
+
+insert into metadata (name, age, address, phone)
+values ('Noah', 29, '875 Fairmont Avenue', '816-772-4660');
+
+insert into metadata (name, age, address, phone)
+values ('Ava', 21, '912 Armory Road', '818-621-3627');
+
+insert into metadata (name, age, address, phone)
+values ('Emma', 30, '3170 Boring Lane', '510-459-9054');
+*/
+select * from metadata;
