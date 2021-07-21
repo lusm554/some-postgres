3529cc924600603b6b753be27a667c2e898c3825
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-21 15:24:44 +0300

    show query time execution

--- compile.sh
+++ compile.sh
@@ -5,4 +5,7 @@
 # More about url connection 
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 
+# Log time sql query
+#psql postgresql://postgres:lusm123@localhost/learn -c '\timing' -f $1
+
 psql postgresql://postgres:lusm123@localhost/learn -f $1
