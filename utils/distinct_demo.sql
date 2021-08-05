bdcc2222e505072c10bca69bcd2b118ea254a14a
loveyousomuch <loveyousomuch554@gmail.com>
2021-08-05 15:56:13 +0300

    set demo table for distinct statement

---
+++ utils/distinct_demo.sql
@@ -0,0 +1,20 @@
+create table distinct_demo (
+  id serial not null primary key,
+  bcolor varchar,
+  fcolor varchar
+);
+
+insert into distinct_demo (bcolor, fcolor)
+values
+  ('red', 'red'),
+  ('red', 'red'),
+  ('red', NULL),
+  (NULL, 'red'),
+  ('red', 'green'),
+  ('red', 'blue'),
+  ('green', 'red'),
+  ('green', 'blue'),
+  ('green', 'green'),
+  ('blue', 'red'),
+  ('blue', 'green'),
+  ('blue', 'blue');
