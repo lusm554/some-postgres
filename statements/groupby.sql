3690df2f3200c775c800f20045a02ec8a0bbfe32
loveyousomuch <loveyousomuch554@gmail.com>
2021-08-08 23:15:31 +0300

    move into grouping-data

--- statements/groupby.sql
+++
@@ -1,6 +0,0 @@
-/*insert into test(name, city)
-values ('smth', 'sf');*/
-
-select count(*), city from test
-group by city
-order by count(*) asc;
--- statements/having.sql
+++
@@ -1,5 +0,0 @@
-select count(city), city from test
-group by city 
-having count(city) > 2;
-
-/* having clause was added to sql bc the where keyword cannot be used with aggregate functions */
