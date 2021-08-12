d84418690f880fa830b442013dff49a519e5fc64
loveyousomuch <loveyousomuch554@gmail.com>
2021-08-12 18:18:28 +0300

    try any, all, exists

--- statements/subquery.sql
+++ statements/subquery.sql
@@ -46,3 +46,72 @@ where
       p.customer_id = c.customer_id
   )
 
+-- ANY Operator
+select title
+from film
+where length >= any(
+  select max(length)
+  from film
+  inner join film_category using(film_id) -- on film.film_id = film_category.film_id
+  group by category_id
+);
+
+-- ALL Operator
+select
+  film_id,
+  title,
+  length
+from
+  film
+where length > all(
+  select
+    round(avg(length), 2)
+  from
+    film
+  group by rating
+)
+order by
+  length;
+
+-- exist
+select
+  first_name,
+  last_name
+from
+  customer c
+where exists(
+  select 1
+  from 
+    payment p
+  where p.customer_id = c.customer_id and
+    amount > 11)
+order by
+  first_name, last_name;
+
+-- not exist
+select
+  first_name,
+  last_name
+from
+  customer c
+where not exists(
+  select 1
+  from 
+    payment p
+  where p.customer_id = c.customer_id and
+    amount > 11)
+order by
+  first_name, last_name;
+
+-- If the subquery returns NULL, EXISTS returns true
+select
+  first_name,
+  last_name
+from
+  customer
+where
+  exists(select null)
+order by
+  first_name,
+  last_name;
+
